6177b681061fcc17ec9a95b1df6679709236cffc
Jason <jason@Jasons-MacBook-Pro.local>
2022-10-05 19:06:24 -0600

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
