0c2f6d89aea81c31aaabd2983bfae2937e301c15
Jason <jason@Jasons-MacBook-Pro.local>
2022-10-09 18:15:57 -0600

    remove import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
